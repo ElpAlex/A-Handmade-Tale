ff877bddea77830c3a4fa8f5e84a02cc19fa79fa
ElpAlex <52038964+ElpAlex@users.noreply.github.com>
2021-08-30 20:39:43 +0100

    Update thimbleOnSeesaw.cs

--- Scripts1/thimbleOnSeesaw.cs
+++ Scripts1/thimbleOnSeesaw.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//A script that changes the mass of the object so that when it is reaches its highest point on the y axis, 
+//it becomes heavy and, when it reaches its lowest height is becomes light.
+//This way the character would jump on the other side of the seesaw and by jumping they could reach a higher point
 public class thimbleOnSeesaw : MonoBehaviour {
 
     Rigidbody rb;
